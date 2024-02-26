4af952381977cc2fb3c49afa7a2b538420952716
Philip Squire <kpsquire@gmail.com>
2024-02-26 18:24:47 -0500

    Updated Reveal for One Image

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ ScrollReveal({ reset: true });
 
 ScrollReveal().reveal('h1', {origin: 'right', distance: '600px'});
 
-ScrollReveal().reveal('.luna1', {
-  rotate: {
-      x: 20,
-      z: 20
-  }
-});
-
+ScrollReveal().reveal('.luna1', {rotate: {x: 40, z: 60}});
 ScrollReveal().reveal('.luna2', { easing: 'steps(4)' });
 ScrollReveal().reveal('.luna3', {scale: 0.85});
 ScrollReveal().reveal('.luna4', {origin: 'bottom', distance: '500px'});
